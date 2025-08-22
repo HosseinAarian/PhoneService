c3762d110a0ba0dc44582fa50967340a9fda7a4b
HosseinArian <hoseinaryan22@gmail.com>
2025-08-22 22:37:08 +0330

    service service

---
+++ PhoneService.Application/Interfaces/IServiceService.cs
@@ -0,0 +1,17 @@
+﻿using PhoneService.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneService.Application.Interfaces;
+
+public interface IServiceService
+{
+	Task<IEnumerable<Service>> GetAllAsync();
+	Task<Service> GetByIdAsync(int id);
+	Task CreateAsync(Service brand);
+	Task UpdateAsync(Service brand);
+	Task DeleteAsync(int id);
+}
---
+++ PhoneService.Application/Services/ServiceService.cs
@@ -0,0 +1,30 @@
+﻿using PhoneService.Application.Interfaces;
+using PhoneService.Core.Entities;
+using PhoneService.Core.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneService.Application.Services;
+
+public class ServiceService : IServiceService
+{
+	private readonly IServiceRepository repository;
+
+	public ServiceService(IServiceRepository repository)
+	{
+		this.repository = repository;
+	}
+
+	public Task<IEnumerable<Service>> GetAllAsync() => repository.GetAllAsync();
+
+	public Task<Service> GetByIdAsync(int id) => repository.GetByIdAsync(id);
+
+	public Task CreateAsync(Service service) => repository.AddAsync(service);
+
+	public Task UpdateAsync(Service service) => repository.UpdateAsync(service);
+
+	public Task DeleteAsync(int id) => repository.DeleteAsync(id);
+}
