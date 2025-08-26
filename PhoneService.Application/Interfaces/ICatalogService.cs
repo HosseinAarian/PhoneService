f83e598af2de3c480b45e6b9369964917ef7b0f1
HosseinArian <hoseinaryan22@gmail.com>
2025-08-27 00:06:55 +0330

    catalog service

---
+++ PhoneService.Application/Interfaces/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿using PhoneService.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneService.Application.Interfaces;
+
+public interface ICatalogService
+{
+	Task<List<CatalogDTO>> GetCatalogs();
+}
---
+++ PhoneService.Application/Services/CatalogService.cs
@@ -0,0 +1,25 @@
+﻿using PhoneService.Application.Interfaces;
+using PhoneService.Core.DTOs;
+using PhoneService.Core.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneService.Application.Services;
+
+public class CatalogService : ICatalogService
+{
+	private readonly ICatalogRepository catalogRepository;
+
+	public CatalogService(ICatalogRepository catalogRepository)
+	{
+		this.catalogRepository = catalogRepository;
+	}
+	public async Task<List<CatalogDTO>> GetCatalogs()
+	{
+		var result = await catalogRepository.GetCatalogs();
+		return result;
+	}
+}
