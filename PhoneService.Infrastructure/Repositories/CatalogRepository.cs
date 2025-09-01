901c9255a18c4bcea9c2bc33d8360b43b9ee8048
HosseinArian <hoseinaryan22@gmail.com>
2025-09-01 22:49:56 +0330

    d

--- PhoneService.Infrastructure/Repositories/CatalogRepository.cs
+++ PhoneService.Infrastructure/Repositories/CatalogRepository.cs
@@ -19,7 +19,7 @@ public class CatalogRepository : ICatalogRepository
 		this.context = context;
 	}
 
-	//c
+	
 	public async Task<List<CatalogDTO>> GetCatalogs()
 	{
 		var result = await context.PhoneBrands
